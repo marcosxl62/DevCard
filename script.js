7c73ded1708547a96002abd3b82e0e610d4d0a0f
Helena Angel <centralexperian12@gmail.com>
2026-01-21 21:12:22 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+console.log("script.js carregou");
+
+const switchEl = document.getElementById("switch");
+
+switchEl.addEventListener("click", () => {
+  console.log("clicou no switch");
+
+  document.documentElement.classList.toggle("light");
+
+  console.log("classes no html:", document.documentElement.className);
+
+  const img = document.querySelector("#profile img");
+
+  if (document.documentElement.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar-light.png");
+    img.setAttribute("alt", "Foto de perfil com fundo claro");
+  } else {
+    img.setAttribute("src", "./assets/avatar.png");
+    img.setAttribute("alt", "Foto de perfil com fundo escuro");
+  }
+});
